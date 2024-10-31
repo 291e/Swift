199fa6e933f6296537e1e324a6829948d2b2d5db
291e <yko0321@naver.com>
2024-10-31 13:35:17 +0900

    Initial commit

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by 이규원 on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
